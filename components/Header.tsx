0f01e5ce65f3e613ebcdb5f1d49b890e607c9d97
Caprice-Instinct <linetwangui04@gmail.com>
2025-03-06 19:13:28 +0300

    setup: clerk auth

---
+++ components/Header.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import { useUser } from "@clerk/nextjs";
+
+const Header = () => {
+  const { user } = useUser();
+  return (
+    <div className="flex items-center justify-between py-4">
+      {user && (
+        <h1 className="text-2xl">
+          {user?.firstName}
+          {`'s`} Space
+        </h1>
+      )}
+
+      {/* Breadcrumbs */}
+      <div>
+        <SignedOut>
+            <SignInButton />
+        </SignedOut>
+        <SignedIn>
+            <UserButton />
+        </SignedIn>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
